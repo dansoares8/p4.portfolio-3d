99c4c7844e6fa794df16b3318bd66cd5f14f7f49
dansoares8 <103952709+dansoares8@users.noreply.github.com>
2025-11-16 22:19:29 -0300

    up

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+    return(
+        <main>
+            <div>
+                <h1>Bem vindo ao meu Portfólio 3d!</h1>
+            </div>
+        </main>
+    )
+}
+
+export default App
